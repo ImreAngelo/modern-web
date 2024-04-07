d3f4e8a94e2c7b17952d87a7fa3933dc40966d99
Imre <imre@angelo.no>
2024-04-07 13:25:59 +0200

    Minor changes

--- services/app/src/app/post/[postID]/page.js
+++ services/app/src/app/post/[postID]/page.js
@@ -13,12 +13,15 @@ export default async function Post({ params }) {
     )
 }
 
+// Metadata for social sharing and search engines
 const metadata = (data, postID) => (
     <>
+        <title>{ data.title }</title>
+        <description>{ data.description }</description>
         <meta property="og:title" content={ data.title }/>
         <meta property="og:description" content={ data.title }/>
         <meta property="og:type" content="article"/>
-        <meta property="og:url" content={ `http://localhost:3000/post/${postID}` }/>
+        <meta property="og:url" content={ `https://localhost/post/${postID}` }/>
         <meta property="og:image" content="https://img.freepik.com/free-vector/gradient-abstract-mountain-background-design_23-2149178797.jpg"/>
         <meta name="twitter:title" content={data.title}/>
         <meta name="twitter:description" content={data.description}/>
@@ -27,25 +30,6 @@ const metadata = (data, postID) => (
         <meta name="X:card" content="summary_large_image"/>
     </>
 )
-
-// // Metadata API
-// export async function generateMetadata({ params }) {
-//     const { postID } = params; 
-//     const data = await populateFromServer(postID);
-
-//     return {
-//         title: data.title,
-//         description: data.description,
-//         other: {
-//             "twitter:title": data.title,
-//             "twitter:description": data.description,
-//             "twitter:image": "https://img.freepik.com/free-vector/gradient-abstract-mountain-background-design_23-2149178796.jpg",
-//             "twitter:card": "summary_large_image",
-//             "X:card": "summary_large_image",
-//             // "twitter:site": "@company_username",
-//         },
-//     }
-// }
 
 // ISR
 export const revalidate = 10
@@ -58,8 +42,10 @@ async function populateFromServer(postID) {
     // const res = await fetch(`${process.env.API_HOST}/${process.env.API_VERSION}/post/random`)
     // const data = await res.json()
 
+    const random = Math.floor(Math.random() * 100);
+
     const data = {
-        id:69, 
+        id:random,
         title:"Sample Page", 
         description:"Lorem ipsum..."
     }
