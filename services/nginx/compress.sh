7a7c6c08f3f6259267f237b29e1820ce271a5f9b
Imre Angelo <ImredeAngelo@users.noreply.github.com>
2024-06-19 01:49:16 +0200

    Minor tweaks to performance

--- services/nginx/compress.sh
+++ services/nginx/compress.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # Compress text files with gzip and brotli
-for f in `find | grep -E "\.js$|\.json$|\.css$|\.html$|\.woff2$|\.txt$"`
+for f in `find | grep -E "\.js$|\.json$|\.css$|\.html$|\.woff2$|\.txt$|\.svg$"`
 do 
     echo "Compressing text: $f"
     gzip -9k "$f"
     brotli -k -Z "$f"
 done
 
-# Compress image files as webp and avif, only in media folder
-for f in `cd media && find | grep -E "\.jpe?g$|\.png$"`
+# Compress image files as webp and avif
+for f in `find | grep -E "\.jpe?g$|\.png$"`
 do 
     echo "Compressing image: $f"
     cwebp -q 90 "$f" -o "$f.webp"
